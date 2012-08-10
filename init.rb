c36717b31238f25892db090a2487cc2c18076bf3
ameya86 <a_house_of_tomato@lion.ocn.ne.jp>
2012-08-10 20:17:31 +0900

    version up

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_query_pulldown do
   name 'Redmine Query Pulldown plugin'
   author 'OZAWA Yasuhiro'
   description 'Insert custom queries link on issues link'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/ameya86/redmine_query_pulldown'
   author_url 'http://blog.livedoor.jp/ameya86/'
 end
